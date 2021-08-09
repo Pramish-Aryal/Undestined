36f0761618fe01c245dd21cd97102ecdb7ca4ad0
Pramish-Aryal <076bct046.pramish@pcampus.edu.np>
2021-08-09 19:04:49 +0545

    Exported stringstar field tilemap

---
+++ data/Stringstar field.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Stringstar field" tilewidth="16" tileheight="16" tilecount="198" columns="18">
+ <image source="stringstar.png" width="288" height="176"/>
+</tileset>
